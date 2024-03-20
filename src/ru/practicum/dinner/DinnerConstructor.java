f0daa3070aa48ab51d3ca4363ef9d177ebd282dd
Alexander Gorobets <alexander.gorobets@me.com>
2024-03-20 11:34:38 +0300

    Написана логика приложения. Для проверки используются готовые массивы данных

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -1,5 +1,66 @@
 package ru.practicum.dinner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class DinnerConstructor {
 
+    HashMap<String, ArrayList<String>> dishes = new HashMap<>();
+    ArrayList<String> dishesNames;
+
+
+    void setDishes(String dishType, String dishName) {
+        System.out.println(dishType + " " + dishName);
+        dishesNames = dishes.get(dishType);
+        if (dishesNames == null) {
+            dishesNames = new ArrayList<>();
+        }
+        dishesNames.add(dishName);
+        dishes.put(dishType, dishesNames);
+        System.out.println(dishes);
+        System.out.println("-------");
+
+    }
+
+    void setDefaultDishes() {
+        dishesNames = new ArrayList<>();
+        dishesNames.add("Яблочный сок");
+        dishesNames.add("Томатный сок");
+        dishesNames.add("Черный чай");
+        dishesNames.add("Зеленый чай");
+        dishesNames.add("Американо");
+        dishesNames.add("Капучино");
+        dishes.put("Напитки", dishesNames);
+
+        dishesNames = new ArrayList<>();
+        dishesNames.add("Куриный суп");
+        dishesNames.add("Борщ");
+        dishesNames.add("Солянка");
+        dishesNames.add("Харчо");
+        dishesNames.add("Шурпа");
+        dishesNames.add("Шулюм");
+        dishes.put("Первые блюда", dishesNames);
+
+        dishesNames = new ArrayList<>();
+        dishesNames.add("Стейк Рибай");
+        dishesNames.add("Котлеты рыбные");
+        dishesNames.add("Котлета по-Киевски");
+        dishesNames.add("Поджарка свиная");
+        dishesNames.add("Печень по-Строгановски");
+        dishesNames.add("Цыпленок Тапака");
+        dishes.put("Вторые блюда", dishesNames);
+
+        dishesNames = new ArrayList<>();
+        dishesNames.add("Картофельное пюре");
+        dishesNames.add("Гречка");
+        dishesNames.add("Рис");
+        dishesNames.add("Ризотто");
+        dishesNames.add("Овощи гриль");
+        dishesNames.add("Овощи на пару");
+        dishes.put("Гарниры", dishesNames);
+
+        System.out.println(dishes);
+        System.out.println("-------");
+
+    }
 }
--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -1,15 +1,20 @@
 package ru.practicum.dinner;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    static DinnerConstructor dc;
+    //static DinnerConstructor dc;
     static Scanner scanner;
+    static Random random;
+    static DinnerConstructor dinnerConstructor = new DinnerConstructor();
 
     public static void main(String[] args) {
-        dc = new DinnerConstructor();
+        // dc = new DinnerConstructor();
         scanner = new Scanner(System.in);
+        random = new Random();
 
         while (true) {
             printMenu();
@@ -20,7 +25,7 @@ public class Main {
                     addNewDish();
                     break;
                 case "2":
-                    generateDishCombo();
+                    generateDishCombo(setDefaultDishTypes()); //TODO
                     break;
                 case "3":
                     return;
@@ -36,30 +41,51 @@ public class Main {
     }
 
     private static void addNewDish() {
-        System.out.println("Введите тип блюда:");
+        /*System.out.println("Введите тип блюда:");
         String dishType = scanner.nextLine();
         System.out.println("Введите название блюда:");
         String dishName = scanner.nextLine();
-
-        // добавьте новое блюдо
+        dinnerConstructor.setDishes(dishType, dishName);// добавьте новое блюдо*/
+        dinnerConstructor.setDefaultDishes();
     }
 
-    private static void generateDishCombo() {
+    private static void generateDishCombo(ArrayList<String> list) {
         System.out.println("Начинаем конструировать обед...");
 
         System.out.println("Введите количество наборов, которые нужно сгенерировать:");
         int numberOfCombos = scanner.nextInt();
         scanner.nextLine();
 
+
         System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). Для завершения ввода введите пустую строку");
+        ArrayList<String> inputtedItems = list;//new ArrayList<>();
+
         String nextItem = scanner.nextLine();
 
         //реализуйте ввод типов блюд
         while (!nextItem.isEmpty()) {
-
+            inputtedItems.add(nextItem);
+            nextItem = scanner.nextLine();
         }
-
+        System.out.println(inputtedItems);
         // сгенерируйте комбинации блюд и выведите на экран
 
+        for (int i = 0; i < numberOfCombos; i++) {
+            ArrayList<String> combo = new ArrayList<>();
+            for (String type : inputtedItems) {
+                int bound = random.nextInt(dinnerConstructor.dishes.get(type).size());
+                combo.add(dinnerConstructor.dishes.get(type).get(bound));
+            }
+            System.out.printf("Комбо %d \n %s \n", (i + 1), combo);//dinnerConstructor.dishes.values());
+        }
+    }
+
+    private static ArrayList<String> setDefaultDishTypes() {
+        ArrayList<String> inputtedDishes = new ArrayList<>();
+        inputtedDishes.add("Первые блюда");
+        inputtedDishes.add("Напитки");
+        inputtedDishes.add("Вторые блюда");
+        inputtedDishes.add("Гарниры");
+        return inputtedDishes;
     }
 }
